332dbeb8b6e31c6ec3e1e1b6b5d7cf7bae35226b
Newton Duarte Cavalcanti <newton_@hotmail.com>
2023-08-25 11:07:31 -0300

    fix: installment class

--- Entities/Installment.cs
+++ Entities/Installment.cs
@@ -3,13 +3,14 @@ class Installment
   public DateTime DueDate { get; set; }
   public double Amount { get; set; }
 
-  public IOnlinePayment _onlinePayment;
-
-  public Installment(int installmentNumber, IOnlinePayment onlinePayment, DateTime dueDate, double amount)
+  public Installment(DateTime dueDate, double amount)
   {
-    _onlinePayment = onlinePayment;
-
     DueDate = dueDate;
-    Amount = _onlinePayment.GeneratePayment(installmentNumber, amount);
+    Amount = amount;
+  }
+
+  public override string ToString()
+  {
+    return $"{DueDate:dd/MM/yyyy} - {Amount:F2}";
   }
 }
