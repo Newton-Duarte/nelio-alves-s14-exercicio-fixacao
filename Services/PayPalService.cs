cd3ec3cb324656e91a2797f1baa320f6419844a6
Newton Duarte Cavalcanti <newton_@hotmail.com>
2023-08-25 11:07:12 -0300

    feat: implement pay pal online payment service interface

--- Services/PayPalService.cs
+++ Services/PayPalService.cs
@@ -1,11 +1,15 @@
-class PayPalService : IOnlinePayment
+class PayPalService : IOnlinePaymentService
 {
-  private const double INSTALLMENT_INTEREST = 0.01;
-  private const double INSTALLMENT_TAX = 0.02;
-  public double GeneratePayment(int installmentNumber, double amount)
+  private const double FeePercentage = 0.02;
+  private const double MonthlyInterest = 0.01;
+
+  public double Interest(double amount, int months)
   {
-    double amountWithInterest = amount + amount * INSTALLMENT_INTEREST * installmentNumber;
-    double amountWithTax = amountWithInterest + amountWithInterest * INSTALLMENT_TAX;
-    return amountWithTax;
+    return amount * MonthlyInterest * months;
+  }
+
+  public double PaymentFee(double amount)
+  {
+    return amount * FeePercentage;
   }
 }
